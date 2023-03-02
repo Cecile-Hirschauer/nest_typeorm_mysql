3a083fc1c08f05c668492c843be386b51543848d
cecile hirschauer <cecile.h.75@gmail.com>
2023-03-02 11:52:29 +0100

    Create mysql db nest + entity Users.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './typeorm/entities/Users';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'cecile',
+      password: '1234',
+      database: 'nest',
+      entities: [Users],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
